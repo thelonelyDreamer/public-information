5e8f499e92155ab1e7e65f76ff3ef9e3f87c2b9e
cleboy <1192136710@qq.com>
2022-06-12 15:59:16 +0800

    auto

--- 脚本/push-all.sh
+++ 脚本/push-all.sh
@@ -1,23 +1,28 @@
 # 保证提交成功
 git_push(){
+    echo -e "\033[31m `pwd` push $1 times  \033[0m"
     git push
     if [ $? -eq 0 ]; then
         echo "succeed"
     else
-        git_push
+        if [ $1 -lt 15 ]; then
+            git_push $[$1+1]
+        else
+            echo `pwd` >> ~/auto-git-err.log
+            echo -e '\r\n' >> ~/auto-git-err.log
+        fi
     fi  
 }
 
 # 自动提交
 auto_commit(){
-    echo -e "\033[31m `pwd` \033[0m"
     git add .
     git commit -m auto
-    git_push
+    git_push 1
 }
 
 # 定义一个根目录数组
-root_arr=("github" "gitee")
+root_arr=("gitee" "github")
 
 # 进入一个目录，如果是仓库目录，则提交，如果不是，则遍历子目录，遍历完成退出
 push_dir(){
@@ -35,6 +40,12 @@ push_dir(){
     fi 
     cd ..
 }
+
+if [ -f ~/auto-git-err.log ]; then
+    cat ~/auto-git-err.log
+    exit -1
+fi 
+
 
 for root_dir in ${root_arr[@]}
 do
